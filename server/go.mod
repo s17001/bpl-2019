587c08f9156d4a985b54e63e23f67c30ea7daace
s17001 <s17001@std.it-college.ac.jp>
2019-05-21 17:01:38 +0900

    serverにデータを送信するところまでできた

--- server/go.mod
+++ server/go.mod
@@ -2,5 +2,6 @@ module server
 
 require (
 	github.com/gin-contrib/cors v1.3.0
+	github.com/gin-gonic/contrib v0.0.0-20190510065052-87e961e51ccc
 	github.com/gin-gonic/gin v1.4.0
 )
--- server/main.go
+++ server/main.go
@@ -1,21 +1,27 @@
 package main // import "server"
 
 import (
-    "server/handler"
+	"server/handler"
 
-    "github.com/gin-contrib/cors"
-    "github.com/gin-gonic/gin"
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/contrib/static"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-    r := gin.Default()
+	r := gin.Default()
 
-    r.Use(cors.New(cors.Config{
-        AllowOrigins: []string{"http://localhost:8080"},
-        AllowMethods: []string{"GET", "POST", "DELETE", "OPTIONS"},
-        AllowHeaders: []string{"*"},
-    }))
+	r.Use(cors.New(cors.Config{
+		AllowOrigins: []string{"http://localhost:8080"},
+		AllowMethods: []string{"GET", "POST", "DELETE", "OPTIONS"},
+		AllowHeaders: []string{"*"},
+	}))
 
-    r.POST("/images", handler.Upload)
-    r.Run(":8888")
+	// 追加!!
+	r.Use(static.Serve("/", static.LocalFile("./images", true)))
+
+	r.GET("/images", handler.List)
+	r.POST("/images", handler.Upload)
+	r.DELETE("/images/:uuid", handler.Delete)
+	r.Run(":8888")
 }
